d23c41067ad9b35c656ac91ee2f5db01f87ea502
Richard Wasonga <rowwasonga@gmail.com>
2025-06-26 00:09:22 +0300

    add text data type to tokenblocklist

--- server/models/token_blocklist.py
+++ server/models/token_blocklist.py
@@ -6,7 +6,7 @@ class TokenBlocklist(db.Model):
     __tablename__ = "token_blocklist"
 
     id = db.Column(db.Integer, primary_key=True)
-    jti = db.Column(db.String, nullable=False, unique=True)
+    jti = db.Column(db.Text, nullable=False, unique=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
